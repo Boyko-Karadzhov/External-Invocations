84616f988d4cc4de51e97acd13d46f8c882cb003
Boyko-Karadzhov <bokovskii@gmail.com>
2015-01-23 00:08:16 +0200

    added NvidiaToolkit assembly

---
+++ Karadzhov.ExternalInvocations.NvidiaToolkit/CudaCompiler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karadzhov.ExternalInvocations.NvidiaToolkit
+{
+    /// <summary>
+    /// Instances of this class invoke the NVIDIA (R) Cuda compiler driver.
+    /// </summary>
+    public class CudaCompiler
+    {
+    }
+}
--- Karadzhov.ExternalInvocations.NvidiaToolkit/Properties/AssemblyInfo.cs
+++ Karadzhov.ExternalInvocations.NvidiaToolkit/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Karadzhov.ExternalInvocations.VisualStudioCommonTools")]
+[assembly: AssemblyTitle("Karadzhov.ExternalInvocations.NvidiaToolkit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Karadzhov.ExternalInvocations.VisualStudioCommonTools")]
+[assembly: AssemblyProduct("Karadzhov.ExternalInvocations.NvidiaToolkit")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,4 +20,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("63a38781-aae7-4de8-9098-ba87150e6516")]
+[assembly: Guid("94689bcb-4a47-4d22-85a0-78ab5700a617")]
